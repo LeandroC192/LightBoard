d757680c7b618cb2ec0d954563b7e7299600756b
LeandroC192 <Leandroc16@nycstudents.net>
2025-04-28 15:46:35 +0000

    Completed Code

--- LightBoard.java
+++ LightBoard.java
@@ -15,10 +15,10 @@ public class LightBoard
       lights = new boolean [numRows][numCols];
       for(int row = 0; row < lights.length; row++)
       {
-         for (int col = 0; col < numRows; col++)
+         for (int col = 0; col < lights[0].length; col++)
          {
-            double temp = Math.random() * 10;
-            if(temp <= 4)
+            double temp = Math.random();
+            if(temp < 0.4)
             {
                lights[row][col] = true;
             }
@@ -29,43 +29,60 @@ public class LightBoard
  * as described in part (b).
  * Precondition: row and col are valid indexes in lights.
  */
+   public double test()
+   {
+      double on = 0;
+      for(boolean[] row : lights)
+      {
+         for(boolean b : row)
+         {
+            if(b) on++;
+         }
+      }
+      return on / (lights.length * lights[0].length);
+   }
+
    public boolean evaluateLight(int row, int col)
    {
-      if(lights[row][col])
+      boolean light = lights[row][col];
+      if(light)
       {
          int temp1 = 0;
-         for (int col1 = 0; col1 < lights.length; col1++)
+         for (int row1 = 0; row1 < lights.length; row1++)
          {
-            if(lights[row][col1])
+            if(lights[row1][col])
             {
                temp1++;
             }
          }
-
-         if(temp1 % 2 != 0)
+         if(light && temp1 % 2 == 0)
          {
-            return (temp1 % 2 != 0);
+            return false;
          }else{
-            return lights[row][col];
+            return light;
          }
       }else{
          int temp2 = 0;
-         for (int col1 = 0; col1 < lights.length; col1++)
+         for (int row1 = 0; row1 < lights.length; row1++)
          {
-            if(lights[row][col1])
+            if(lights[row1][col])
             {
                temp2++;
             }
          }
-
-         if(temp2 % 3 == 0)
+         if(!light && temp2 % 3 == 0)
          {
             return (temp2 % 3 == 0);
          }else{
-            return lights[row][col];
+            return light;
          }
       } 
     /* to be implemented in part (b) */ 
+   }
+
+   public void setLights(boolean[][] arr)
+   {
+      lights = arr;
    }
  // There may be additional instance variables, constructors, and methods not shown.
 } 
